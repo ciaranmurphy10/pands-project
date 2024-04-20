6cad469f283ed7ac73e87f814dd416dc39cd1731
Ciaran Murphy <ciaranmurphy10@gmail.com>
2024-04-20 14:55:30 +0100

    Added comment and column names.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pands-project
+# 23-24: 8632 -- PROGRAMMING AND SCRIPTING. 
+
+# Project
+
+
--- project.py
+++ project.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
-iris_df = pd.read_csv("C:\\Users\\ciara\\Documents\\college\\pands-project\\iris.data")
+# Although our data file is in .data format, the underlying data is in .csv format. 
+# We can use read_csv() function from the pandas to import our data into a pandas DataFrame. 
+iris_df = pd.read_csv("C:\\Users\\ciara\\Documents\\college\\pands-project\\iris.data", names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
