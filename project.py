6320558df53b3ca6dfb7a3013a1fc3f5a6256812
Ciaran Murphy <ciaranmurphy10@gmail.com>
2024-05-03 10:26:58 +0100

    Updated custom_hist() function.

--- project.py
+++ project.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 # Will need add functionality later for other users to run this file. 
 iris_dir = "C:\\Users\\ciara\\Documents\\college\\pands-project"
 
+
 # Although our data file is in .data format, the underlying data is in .csv format. 
 # Use the read_csv() function from the pandas library to import our data into a pandas DataFrame. 
 iris_df = pd.read_csv(f"{iris_dir}\\iris.data", names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species_type"])
+
 
 # Open a file called iris_summary.txt, creating it if necessary, in write mode. 
 with open(f"{iris_dir}\\iris_summary.txt", "w") as file:  
@@ -22,19 +24,38 @@ with open(f"{iris_dir}\\iris_summary.txt", "w") as file:
     file.write(iris_df.describe().to_string())
     file.write("\n\n") # Add empty lines. 
 
+
 sns.pairplot(iris_df) # Create a scatter plot matrix using Seaborn's pairplot function. 
 plt.savefig(f"{iris_dir}\\iris_pair_plot.png") # Save the plot to a png file. 
 
-# Define a custom function to create a custom histogram and save it to a png file.
-def custom_hist(variable, output_dir):
 
-    # Create the figure and axes
+def custom_hist(variable, output_dir, number_of_bins = 30, bin_colour = "lightsteelblue"):
+    """
+    This function creates a custom styled histogram and outputs it to a png file.  
+
+    Args:
+        variable (str): The numberical variable you would like to use for your histogram. 
+        output_dir (str): The directory to save the histogram png to. 
+        number_of_bins (int): Number of bins you would like in your histogram. The default is 30. 
+        bin_colour (str): The colour you would like the histogram bins to be. The default is lightsteelblue. 
+        
+    Output:
+        matplotlib.figure.Figure: The fig object that we created. 
+    """
+    # Create the figure and axes.
     fig, ax = plt.subplots()
 
-    # Create a histogram of the inputted variable.
+    # Create a histogram of the inputted variable using the seaborn library.
     sns.histplot(iris_df[variable], 
-                 bins = 30, # Divide the histogram into 30 bins. 
-                 ax = ax) # Specify that the histogram is to be drawn on the ax that we already created.
+                 bins = number_of_bins, # Divide the histogram into the chosen number of bins. 
+                 color = bin_colour, # Set the bin colour. 
+                 ax = ax # Specify that the histogram is to be drawn on the ax that we already created. 
+                 )
     
+    # Remove spaces and use title case of variable name for the plot title.
+    ax.set_title(f"{variable.replace("_", " ").title()} Histogram")
+
     # Save the plot to the chosen directory, creating a dynamic name based on the variable. 
     plt.savefig(f"{output_dir}\\{variable}_histogram.png") 
+
+    return fig
